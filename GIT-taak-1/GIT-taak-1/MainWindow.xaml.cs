bf1210324a568fa8fa3ddc20cacf4e8e30cff0f3
Jaysen Stas <jaysen.stas@student.pxl.be>
2024-10-07 11:56:44 +0200

    KLEUR-0.4 KLEUR-0.5

--- GIT-taak-1/GIT-taak-1/MainWindow.xaml.cs
+++ GIT-taak-1/GIT-taak-1/MainWindow.xaml.cs
@@ -1,13 +1,8 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GIT_taak_1
 {
@@ -16,9 +11,45 @@ namespace GIT_taak_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Kleurcodes en informatie
+        string redCode = "#FF0000";
+        string redInfo = "Rood is de kleur van warmte";
+
+        string greenCode = "#008000";
+        string greenInfo = "Groen is de kleur van genezing";
+
+        string yellowCode = "#FFFF00";
+        string yellowInfo = "Geel is de kleur van levenslust";
+
+        string blueCode = "#0000FF";
+        string blueInfo = "Blauw is de kleur van intelligentie";
+
         public MainWindow()
         {
             InitializeComponent();
         }
-    }
-}
+
+        // Event handlers voor klik op kleuren
+        private void redImage_Click(object sender, RoutedEventArgs e)
+        {
+            codeTextbox.Text = redCode;
+            infoTextbox.Text = redInfo;
+        }
+
+        private void greenImage_Click(object sender, RoutedEventArgs e)
+        {
+            codeTextbox.Text = greenCode;
+            infoTextbox.Text = greenInfo;
+        }
+
+        private void yellowImage_Click(object sender, RoutedEventArgs e)
+        {
+            codeTextbox.Text = yellowCode;
+            infoTextbox.Text = yellowInfo;
+        }
+
+        private void blueImage_Click(object sender, RoutedEventArgs e)
+        {
+            codeTextbox.Text = blueCode;
+            infoTextbox.Text = blueInfo;
+        }
